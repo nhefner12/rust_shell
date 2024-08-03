2b625fe2a2765ed5d04050d0bd28a10c540db3e6
nhefner12 <nick.g.hefner@gmail.com>
2024-08-02 21:35:18 -0700

    inital rust shell command line and arg parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::{env, io::{stdin, stdout, Write}, path::Path, process::Command};
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print!(" >");
+        stdout().flush();
+
+
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        let mut parts = input.trim().split_whitespace();
+
+        let command = parts.next().unwrap();
+        let args = parts;
+
+        
+
+        match command {
+            "cd" => {
+                // default to '/' as new directory if one was not provided
+                let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                let root = Path::new(new_dir);
+                if let Err(e) = env::set_current_dir(&root) {
+                    eprintln!("{}", e);
+                }
+            },
+            command => {
+                let mut child = Command::new(command)
+                    .args(args)
+                    .spawn()
+                    .unwrap();
+
+                child.wait();
+            }
+        }
+        
+    };
 }
